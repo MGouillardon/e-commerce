3d73807dc430bd530a7761c7f72ec4cd241544b3
Maxime Gouillardon <maxime.gouillardon@gmail.com>
2022-11-22 19:54:19 +0100

    fix: querySelectorAll js

--- resources/scripts/addToCart.js
+++ resources/scripts/addToCart.js
@@ -1,17 +1,18 @@
-let addBtn = document.querySelector(".add");
+let addButtons = document.querySelectorAll(".add");
 let cart = document.getElementById("cart-js");
 
-addBtn.addEventListener("click", async () => {
-  let response = await fetch("/addCart");
-  let data = await response.json();
-  let quantityCart = Object.values(data);
-  setCartQuantity(quantityCart);
-});
+for (let addBtn of addButtons) {
+  addBtn.addEventListener("click", async () => {
+    let response = await fetch("/addCart");
+    let data = await response.json();
+    let quantityCart = Object.values(data);
+    setCartQuantity(quantityCart);
+  });
+}
 
 function setCartQuantity(quantityCart) {
-    let cartQuantity = document.createElement('div');
-    cartQuantity.classList.add('cart-quantity');
-    cart.appendChild(cartQuantity);
-    cartQuantity.innerHTML = quantityCart;
-
+  let cartQuantity = document.createElement("div");
+  cartQuantity.classList.add("cart-quantity");
+  cart.appendChild(cartQuantity);
+  cartQuantity.innerHTML = quantityCart;
 }
